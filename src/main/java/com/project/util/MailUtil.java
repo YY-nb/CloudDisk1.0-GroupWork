24cfc33047936851b5638c13215bb1a55a7e259d
YY-nb <916295483@qq.com>
2021-03-07 14:05:07 +0800

    完善部分功能

--- src/main/java/com/project/util/MailUtil.java
+++ src/main/java/com/project/util/MailUtil.java
@@ -22,7 +22,7 @@ public class MailUtil {
 
             email.addTo(userEmail);//设置收件人
             email.setFrom("1571727223@qq.com","网盘");//设置发送人，邮箱和用户名，发送人的邮箱为自己的，用户名可以随便填
-            email.setAuthentication("1571727223@qq.com","otjsylnkvmmtidfb");//设置发送人到的邮箱和用户名和授权码(授权码是自己设置的)
+            email.setAuthentication("1571727223@qq.com","");//设置发送人到的邮箱和用户名和授权码(授权码是自己设置的)
             email.setSubject("网盘验证码");//设置发送主题
             email.setMsg(String.format("欢迎注册xx网盘，这是注册验证码%s，请于2分钟内填写",authCode));//设置发送内容
             email.send();//进行发送
