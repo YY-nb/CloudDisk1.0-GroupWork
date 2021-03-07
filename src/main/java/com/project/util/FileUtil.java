7369c3b8433dad70c8061027acdba7ce53acba6d
YY-nb <916295483@qq.com>
2021-03-07 23:54:23 +0800

    完善部分功能

--- src/main/java/com/project/util/FileUtil.java
+++ src/main/java/com/project/util/FileUtil.java
@@ -15,7 +15,8 @@ public class FileUtil {
      * @throws IOException
      */
     public  static void uploadFile(MultipartFile myFile,String path,String fileName) throws IOException {
-        File file =new File(path+"/"+fileName);
+        new File(path).mkdirs();
+        File file =new File(path,fileName);
         myFile.transferTo(file);
     }
 
