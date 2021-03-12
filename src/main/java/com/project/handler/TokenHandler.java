e318d4e455dc3036968657517b069353fcfb438a
YY-nb <916295483@qq.com>
2021-03-12 21:37:59 +0800

    完善部分功能

--- src/main/java/com/project/handler/TokenHandler.java
+++ src/main/java/com/project/handler/TokenHandler.java
@@ -21,7 +21,7 @@ public class TokenHandler implements HandlerInterceptor {
         ResultVo result=new ResultVo();
         //登录或注册时的请求直接放行
         if(("/user/login".equals(path))||("/user/register".equals(path))||
-                ("/user/getCode".equals(path))||("/user/checkName".equals(path))||"/test".equals(path)
+                ("/user/getCode".equals(path))||("/user/checkName".equals(path))
                 ||"/admin/login".equals(path)){
             return true;
         }
